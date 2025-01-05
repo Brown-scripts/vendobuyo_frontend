3e26e468ee7eeab0f06eb8e4d93c5c124616e4bb
Brown-scripts <nikkibrowm297@gmail.com>
2025-01-05 23:12:31 +0000

    added client

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { Link } from "react-router-dom";
+import client from '../api/client';
 
 function Products() {
   const [products, setProducts] = useState([]);
